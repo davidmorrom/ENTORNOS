20e014e2b6d2f7d42fd724afdb45633aac135650
David Moreno Práctica Git <david.morrom.contact@gmail.com>
2023-03-24 01:22:04 +0100

    Mensaje del commit en David_secundaria2

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -2,7 +2,7 @@ class TestCuentaBancaria
 {
 	public static void main(String args[])
 	{
-		CuentaBancaria cuenta = new CuentaBancaria("0123456789","Nombre Apellidos",1000,5);
+		CuentaBancaria cuenta = new CuentaBancaria("0123456789","David Moreno",1000,5);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
